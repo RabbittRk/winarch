421e0e75f70ac72978e11065e0cb2e9b86ad8f7b
Balaji.R <Balaji.R@winjit.com>
2026-03-12 21:54:13 +0530

    refactor(auth): simplify AuthResponse model by removing Freezed dependency
    - Replaced the Freezed implementation of AuthResponse with a manual class definition.
    - Removed unused generated files related to Freezed.
    - Updated the pubspec.yaml to eliminate Freezed dependencies.

--- lib/features/auth/data/models/auth_response.dart
+++ lib/features/auth/data/models/auth_response.dart
@@ -1,22 +1,108 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+class AuthResponse {
+  factory AuthResponse.fromJson(Map<String, dynamic> json) {
+    return AuthResponse(
+      accessToken: json['accessToken'],
+      refreshToken: json['refreshToken'],
+      id: json['id'],
+      username: json['username'],
+      email: json['email'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      gender: json['gender'],
+      image: json['image'],
+    );
+  }
+  const AuthResponse({
+    required this.accessToken,
+    required this.refreshToken,
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.firstName,
+    required this.lastName,
+    required this.gender,
+    required this.image,
+  });
 
-part 'auth_response.freezed.dart';
-part 'auth_response.g.dart';
+  final String accessToken;
+  final String refreshToken;
+  final int id;
+  final String username;
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String gender;
+  final String image;
 
-@freezed
-abstract class AuthResponse with _$AuthResponse {
-  const factory AuthResponse({
-    @JsonKey(name: 'accessToken') required String accessToken,
-    @JsonKey(name: 'refreshToken') required String refreshToken,
-    @JsonKey(name: 'id') required int id,
-    @JsonKey(name: 'username') required String username,
-    @JsonKey(name: 'email') required String email,
-    @JsonKey(name: 'firstName') required String firstName,
-    @JsonKey(name: 'lastName') required String lastName,
-    @JsonKey(name: 'gender') required String gender,
-    @JsonKey(name: 'image') required String image,
-  }) = _AuthResponse;
+  AuthResponse copyWith({
+    String? accessToken,
+    String? refreshToken,
+    int? id,
+    String? username,
+    String? email,
+    String? firstName,
+    String? lastName,
+    String? gender,
+    String? image,
+  }) {
+    return AuthResponse(
+      accessToken: accessToken ?? this.accessToken,
+      refreshToken: refreshToken ?? this.refreshToken,
+      id: id ?? this.id,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      gender: gender ?? this.gender,
+      image: image ?? this.image,
+    );
+  }
 
-  factory AuthResponse.fromJson(Map<String, dynamic> json) =>
-      _$AuthResponseFromJson(json);
+  Map<String, dynamic> toJson() {
+    return {
+      'accessToken': accessToken,
+      'refreshToken': refreshToken,
+      'id': id,
+      'username': username,
+      'email': email,
+      'firstName': firstName,
+      'lastName': lastName,
+      'gender': gender,
+      'image': image,
+    };
+  }
+
+  @override
+  String toString() {
+    return '''AuthResponse(accessToken: $accessToken, refreshToken: $refreshToken, id: $id, username: $username, email: $email, firstName: $firstName, lastName: $lastName, gender: $gender, image: $image)''';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is AuthResponse &&
+        other.accessToken == accessToken &&
+        other.refreshToken == refreshToken &&
+        other.id == id &&
+        other.username == username &&
+        other.email == email &&
+        other.firstName == firstName &&
+        other.lastName == lastName &&
+        other.gender == gender &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return accessToken.hashCode ^
+        refreshToken.hashCode ^
+        id.hashCode ^
+        username.hashCode ^
+        email.hashCode ^
+        firstName.hashCode ^
+        lastName.hashCode ^
+        gender.hashCode ^
+        image.hashCode;
+  }
 }
